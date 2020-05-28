9aaf0094236d6f2b73db54d6ac5301dc7d9eecfa
DrMikeG <Mike@admins-iMac.local>
2020-05-28 08:53:55 +0100

    Working nano sketch using pins 3 and 5 for GPS

--- NexStarGPS-master/examples/gps_device/gps_device.ino
+++ NexStarGPS-master/examples/gps_device/gps_device.ino
@@ -1,21 +1,36 @@
-#include <TinyGPS.h>
-#include "NexStarGPS.h"
-#include "soss.h"
-#include "ross.h"
+//#include <TinyGPS.h>
+#include <TinyGPS++.h>
+//#include "NexStarGPS.h"
+//#include "soss.h"
+//#include "ross.h"
+#include <SoftwareSerial.h>
 
-#define RX_PIN 3
-#define TX_PIN 5
+/*
+ * This sketch is just a proof right now. It is programmed against my nano clone
+ * Board: Arduino nano
+ * Processor: AtMega329P (old boot loader)
+ * Programmer: AVR ISP
+ * The GPS is connected Blue wire (RX) to pin D5 - via a voltage divider (20k to ground, 10k to pin D5)
+ * The GPS purple wire (TX) is connected to pin D3
+ * This sketch is using softwareSerial and TinyGPS++
+ */
+
+
+//#define RX_PIN 3
+//#define TX_PIN 5
 
 #define SIGNAL_PIN 9
 #define LED_PIN 13
 
-ross mountserial(RX_PIN);
-soss sendmountserial(TX_PIN);
+// The serial connection to the GPS device
+static const int RXPin = 3, TXPin = 5;
+static const uint32_t GPSBaud = 9600;
+SoftwareSerial ss(RXPin, TXPin);
+//ross gpsserial(RX_PIN);
+//soss gpssendserial(TX_PIN);
 
-TinyGPS gps;
-
-NexstarMessageReceiver msg_receiver;
-NexstarMessageSender msg_sender(&gps);
+// The TinyGPS++ object
+TinyGPSPlus gps;
 
 int ledState = LOW;             // ledState used to set the LED
 long previousMillis = 0;        // will store last time LED was updated
@@ -26,102 +41,193 @@ long interval_nolock = 150;           // interval at which to blink (millisecond
 long interval_lock = 5;
 
 boolean haveLock = false;
+// For stats that happen every 5 seconds
+unsigned long last = 0UL;
+
 
 void setup()
 {
 	// GPS module speed
-	Serial.begin(57600);
+// Test output
+  Serial.begin(115200);
+
+   // GPS input/output
+  ss.begin(GPSBaud);
+
+
 	pinMode(LED_PIN, OUTPUT);
 	digitalWrite(LED_PIN, LOW);
 	pinMode(SIGNAL_PIN, OUTPUT);
 	digitalWrite(SIGNAL_PIN, LOW);
-	pinModeTri(RX_PIN);
-	pinModeTri(TX_PIN);
-	mountserial.begin(19200);
-	msg_receiver.reset();
+	//pinModeTri(RX_PIN);
+	//pinModeTri(TX_PIN);
 }
+
+void printGPSData()
+{
+  // Dispatch incoming characters
+  while (ss.available() > 0)
+    gps.encode(ss.read());
+
+  if (gps.location.isUpdated())
+  {
+    Serial.print(F("LOCATION   Fix Age="));
+    Serial.print(gps.location.age());
+    Serial.print(F("ms Raw Lat="));
+    Serial.print(gps.location.rawLat().negative ? "-" : "+");
+    Serial.print(gps.location.rawLat().deg);
+    Serial.print("[+");
+    Serial.print(gps.location.rawLat().billionths);
+    Serial.print(F(" billionths],  Raw Long="));
+    Serial.print(gps.location.rawLng().negative ? "-" : "+");
+    Serial.print(gps.location.rawLng().deg);
+    Serial.print("[+");
+    Serial.print(gps.location.rawLng().billionths);
+    Serial.print(F(" billionths],  Lat="));
+    Serial.print(gps.location.lat());
+    Serial.print(F(" Long="));
+    Serial.print(gps.location.lng());
+    Serial.print('\n');
+  }
+
+  else if (gps.date.isUpdated())
+  {
+    Serial.print(F("DATE       Fix Age="));
+    Serial.print(gps.date.age());
+    Serial.print(F("ms Raw="));
+    Serial.print(gps.date.value());
+    Serial.print(F(" Year="));
+    Serial.print(gps.date.year());
+    Serial.print(F(" Month="));
+    Serial.print(gps.date.month());
+    Serial.print(F(" Day="));
+    Serial.println(gps.date.day());
+  }
+
+  else if (gps.time.isUpdated())
+  {
+    Serial.print(F("TIME       Fix Age="));
+    Serial.print(gps.time.age());
+    Serial.print(F("ms Raw="));
+    Serial.print(gps.time.value());
+    Serial.print(F(" Hour="));
+    Serial.print(gps.time.hour());
+    Serial.print(F(" Minute="));
+    Serial.print(gps.time.minute());
+    Serial.print(F(" Second="));
+    Serial.print(gps.time.second());
+    Serial.print(F(" Hundredths="));
+    Serial.println(gps.time.centisecond());
+  }
+
+  else if (gps.speed.isUpdated())
+  {
+    Serial.print(F("SPEED      Fix Age="));
+    Serial.print(gps.speed.age());
+    Serial.print(F("ms Raw="));
+    Serial.print(gps.speed.value());
+    Serial.print(F(" Knots="));
+    Serial.print(gps.speed.knots());
+    Serial.print(F(" MPH="));
+    Serial.print(gps.speed.mph());
+    Serial.print(F(" m/s="));
+    Serial.print(gps.speed.mps());
+    Serial.print(F(" km/h="));
+    Serial.println(gps.speed.kmph());
+  }
+
+  else if (gps.course.isUpdated())
+  {
+    Serial.print(F("COURSE     Fix Age="));
+    Serial.print(gps.course.age());
+    Serial.print(F("ms Raw="));
+    Serial.print(gps.course.value());
+    Serial.print(F(" Deg="));
+    Serial.println(gps.course.deg());
+  }
+
+  else if (gps.altitude.isUpdated())
+  {
+    Serial.print(F("ALTITUDE   Fix Age="));
+    Serial.print(gps.altitude.age());
+    Serial.print(F("ms Raw="));
+    Serial.print(gps.altitude.value());
+    Serial.print(F(" Meters="));
+    Serial.print(gps.altitude.meters());
+    Serial.print(F(" Miles="));
+    Serial.print(gps.altitude.miles());
+    Serial.print(F(" KM="));
+    Serial.print(gps.altitude.kilometers());
+    Serial.print(F(" Feet="));
+    Serial.println(gps.altitude.feet());
+  }
+
+  else if (gps.satellites.isUpdated())
+  {
+    Serial.print(F("SATELLITES Fix Age="));
+    Serial.print(gps.satellites.age());
+    Serial.print(F("ms Value="));
+    Serial.print(gps.satellites.value());
+  }
+
+  else if (gps.hdop.isUpdated())
+  {
+    Serial.print(F("HDOP       Fix Age="));
+    Serial.print(gps.hdop.age());
+    Serial.print(F("ms Value="));
+    Serial.println(gps.hdop.value());
+  }
+
+  else if (millis() - last > 5000)
+  {
+    Serial.println();
+    if (gps.location.isValid())
+    {
+      static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
+      double distanceToLondon =
+        TinyGPSPlus::distanceBetween(
+          gps.location.lat(),
+          gps.location.lng(),
+          LONDON_LAT, 
+          LONDON_LON);
+      double courseToLondon =
+        TinyGPSPlus::courseTo(
+          gps.location.lat(),
+          gps.location.lng(),
+          LONDON_LAT, 
+          LONDON_LON);
+
+      Serial.print(F("LONDON     Distance="));
+      Serial.print(distanceToLondon/1000, 6);
+      Serial.print(F(" km Course-to="));
+      Serial.print(courseToLondon, 6);
+      Serial.print(F(" degrees ["));
+      Serial.print(TinyGPSPlus::cardinal(courseToLondon));
+      Serial.println(F("]"));
+    }
+
+    Serial.print(F("DIAGS      Chars="));
+    Serial.print(gps.charsProcessed());
+    Serial.print(F(" Sentences-with-Fix="));
+    Serial.print(gps.sentencesWithFix());
+    Serial.print(F(" Failed-checksum="));
+    Serial.print(gps.failedChecksum());
+    Serial.print(F(" Passed-checksum="));
+    Serial.println(gps.passedChecksum());
+
+    if (gps.charsProcessed() < 10)
+      Serial.println(F("WARNING: No GPS data.  Check wiring."));
+
+    last = millis();
+    Serial.println();
+  }
+}
+
+
 
 void loop()
 {
-	unsigned long fix_age;
-	if (mountserial.available())
-	{
-		int c = mountserial.read();
-		//Serial.println(c, HEX);
-		if (msg_receiver.process(c))
-		{
-			if (msg_sender.handleMessage(&msg_receiver))
-			{
-				digitalWrite(LED_PIN, HIGH);
-				mountserial.end();
-				delay(100);
-				sendmountserial.begin(19200);
-				msg_sender.send(&sendmountserial);
-				sendmountserial.end();
-				pinModeTri(RX_PIN);
-				pinModeTri(TX_PIN);
-				mountserial.begin(19200);
-				digitalWrite(LED_PIN, LOW);
-			}
-		}
-	}
-
-	if (Serial.available())
-	{
-		char c = Serial.read();
-		gps.encode(c);
-
-		gps.get_datetime(NULL, NULL, &fix_age);
-
-		if ((fix_age == gps.GPS_INVALID_AGE) || (fix_age > 5000) || (gps.satellites() == gps.GPS_INVALID_SATELLITES) || (gps.satellites() < 4))
-		{
-			haveLock = false;
-		}
-		else
-		{
-			haveLock = true;
-		}
-	}
-
-	unsigned long currentMillis = millis();
-
-	if (haveLock)
-	{
-		if (currentMillis - previousMillis > interval_lock)
-		{
-			previousMillis = currentMillis;
-			static unsigned int val = 21;
-			static int dir = 1;
-			analogWrite(SIGNAL_PIN, val);
-			if ((val == 255) || (val == 20))
-			{
-				dir = dir * (-1);
-			}
-			val = val + dir;
-		}
-
-	}
-	else
-	{
-		if (currentMillis - previousMillis > interval_nolock)
-		{
-			// save the last time you blinked the LED
-			previousMillis = currentMillis;
-
-			// if the LED is off turn it on and vice-versa:
-			if (ledState == LOW)
-			{
-				ledState = HIGH;
-			}
-			else
-			{
-				ledState = LOW;
-			}
-
-
-			digitalWrite(SIGNAL_PIN, ledState);
-		}
-	}
+   printGPSData();
 }
 
 inline void pinModeTri(int pin)

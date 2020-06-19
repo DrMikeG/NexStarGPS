37fbfa8668d75bbac7b0499c75a8984dc9f68833
DrMikeG <Mike@admins-iMac.local>
2020-06-19 22:40:42 +0100

    minimised for port into gps code

--- ssd1306_minimal/ssd1306_minimal.ino
+++ ssd1306_minimal/ssd1306_minimal.ino
@@ -14,65 +14,17 @@ void setup() {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
-
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(2000); // Pause for 2 seconds
-
-  // Clear the buffer
   display.clearDisplay();
 }
+
+int i = 0; 
 
 void loop() {
 
-  testdrawchar();      // Draw characters of the default font
-  delay(2000);
- 
-  testdrawAll(100);
-  delay(2000);
   
-  testdrawcircle();    // Draw circles (outlines)
+  testdrawAll(i);
   delay(2000);
-}
-
-
-void testdrawrect(void) {
-  display.clearDisplay();
-
-  for(int16_t i=0; i<display.height()/2; i+=2) {
-    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, SSD1306_WHITE);
-    display.display(); // Update screen with each newly-drawn rectangle
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-
-void testdrawcircle(void) {
-  display.clearDisplay();
-
-  for(int16_t i=0; i<max(display.width(),display.height())/2; i+=2) {
-    display.drawCircle(display.width()/2, display.height()/2, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfillcircle(void) {
-  display.clearDisplay();
-
-  for(int16_t i=max(display.width(),display.height())/2; i>0; i-=3) {
-    // The INVERSE color is used so circles alternate white/black
-    display.fillCircle(display.width() / 2, display.height() / 2, i, SSD1306_INVERSE);
-    display.display(); // Update screen with each newly-drawn circle
-    delay(1);
-  }
-
-  delay(2000);
+  i++;
 }
 
 void drawString(char* toDraw)
@@ -103,26 +55,5 @@ void testdrawAll(int satelites)
   drawString("satelites:");
   drawInt(satelites);
   display.display();
-  
-   
-
 }
 
-void testdrawchar(void) {
-  display.clearDisplay();
-
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0, 0);     // Start at top-left corner
-  display.cp437(true);         // Use full 256 char 'Code Page 437' font
-
-  // Not all the characters will fit on the display. This is normal.
-  // Library will draw what it can and the rest will be clipped.
-  for(int16_t i=0; i<256; i++) {
-    if(i == '\n') display.write(' ');
-    else          display.write(i);
-  }
-
-  display.display();
-  delay(2000);
-}
